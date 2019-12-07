af7786cfa526a82751c192166b20d5bf99f55fdc
boncheol <boncheol@boncheolui-MacBookPro.local>
2019-12-08 00:40:32 +0900

    Web Template Update
    웹 템플릿만 작성

--- kickstarterAnalysis/mainapp/views.py
+++ kickstarterAnalysis/mainapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
 
 def index(request):
-    return render(request, 'index.html')
+    input_file = "./ks-projects-201612.csv"
+    data_frame = pd.read_csv(input_file, 'utf-8', header=0, index_col=0, engine='python')
+    print(data_frame.columns)
+    # data_frame.sort_values(by=['goal'], axis=0)
+    # corr_heatmap = sns.heatmap(data = data_frame.corr(), annot=True, fmt = '.2f', linewidths=.5, cmap='Blues')
+    # corr_heatmap.savefig("output.png")
+    return render(request, 'index.html', {'dataframe' : data_frame})
 # Create your views here.
